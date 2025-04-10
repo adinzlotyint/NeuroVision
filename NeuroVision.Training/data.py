066fb2cb2f34cdf98bf16cb2078c8dc193ae698d
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-04-10 17:08:43 +0200

    refactor: Replaced tensorflow with torchvision

--- NeuroVision.Training/data.py
+++ NeuroVision.Training/data.py
@@ -1,16 +1,23 @@
-import tensorflow as tf
-import keras
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+from config import BATCH_SIZE, VALIDATION_SPLIT
 
 def load_data():
-    # Loading the CIFAR-10 collection
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
     
-    # Normalization - scaling pixels to the range [0, 1]
-    x_train = x_train.astype('float32') / 255.0
-    x_test  = x_test.astype('float32') / 255.0
-
-    # One-hot encoding labels (10 classes)
-    y_train = tf.keras.utils.to_categorical(y_train, 10)
-    y_test  = tf.keras.utils.to_categorical(y_test, 10)
-
-    return (x_train, y_train), (x_test, y_test)
+    dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    
+    total_train = len(dataset)
+    val_size = int(total_train * VALIDATION_SPLIT)
+    train_size = total_train - val_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=2)
+    
+    return train_loader, val_loader, test_loader
