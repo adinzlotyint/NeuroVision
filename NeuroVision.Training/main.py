3db5924f6342a72a8ae02788cd6b76d061112ec8
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-04-10 17:17:46 +0200

    refactor: Modified model name and model import

--- NeuroVision.Training/main.py
+++ NeuroVision.Training/main.py
@@ -1,16 +1,16 @@
-from models import create_simple_cnn
+from models import BasicCNN
 from train import train_model
 from utils import plot_history
 
 def main():
-    model_name = "SimpleCNN"
-    model = create_simple_cnn()
+    model_name = "BasicCNN"
+    model = BasicCNN()
     history, test_acc, test_loss = train_model(model, model_name)
     
     print(f"\nFinal Results for {model_name}:")
     print(f"Test Accuracy: {test_acc:.4f}")
     print(f"Test Loss: {test_loss:.4f}")
-
+    
     plot_history(history, model_name)
 
 if __name__ == '__main__':
