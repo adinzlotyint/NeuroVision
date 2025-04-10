40f88e4d7e33fc3c7044abf767f59b938dfc8ce1
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-04-10 17:16:43 +0200

    refactor: Change arg passing

--- NeuroVision.Training/utils.py
+++ NeuroVision.Training/utils.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 def plot_history(history, model_name):
     plt.figure(figsize=(12, 8))
-    plt.plot(history.history['val_accuracy'], label=f'{model_name} - Val Accuracy')
+    plt.plot(history['val_accuracy'], label=f"{model_name} - Val Accuracy")
     plt.title(f"Validation Accuracy for {model_name}")
     plt.xlabel("Epoch")
     plt.ylabel("Validation Accuracy")
