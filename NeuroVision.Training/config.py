e69e32d809007809dee125605889e32ecc4d6cb9
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-04-10 16:57:46 +0200

    refactor: Add device variable for selecting cpu/gpu

--- NeuroVision.Training/config.py
+++ NeuroVision.Training/config.py
@@ -1,7 +1,11 @@
-# Training hyperparameters
-EPOCHS = 10
+# Hyperparameters
+EPOCHS = 50
 BATCH_SIZE = 64
 VALIDATION_SPLIT = 0.1
 
-#Save path for models
-MODEL_SAVE_PATH = "./Models/{}-model.h5"
+# Save path for models
+MODEL_SAVE_PATH = "./Models/{}-model.pth"
+
+# device
+import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
