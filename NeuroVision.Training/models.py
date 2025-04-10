d34f6fb6f516d36663f14a1661467ad3a40731e3
adinzlotyint <adrian.kamil.zietek@gmail.com>
2025-04-10 16:21:37 +0200

    moving input_shape to the Input() layer

--- NeuroVision.Training/models.py
+++ NeuroVision.Training/models.py
@@ -5,7 +5,8 @@ def create_simple_cnn():
     #Simple CNN model to classify 10 classes on 32x32 RGB data
     model = models.Sequential([
         #First convolution layer - detects basic features
-        layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+        layers.Input(shape=(32,32,3)),
+        layers.Conv2D(32, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)), #Pooling, reducing img by half
         layers.Conv2D(64, (3, 3), activation='relu'),
         layers.MaxPooling2D((2, 2)), #Pooling, reducing img by half
